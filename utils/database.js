9e378619668349c3d1b0a1e5ddedeb0d33ebe746
JAY GUPTA <jaygupta5474@gmail.com>
2024-04-26 12:18:20 +0530

    database

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("omrusermanagement", "root", "root", {
+const sequelize = new Sequelize("omrscanner", "root", "root", {
   dialect: "mysql",
   host: "localhost",
 });
