da57c2401be3177a2b95e798316d13210732c6eb
krgarav <krgarav@gmail.com>
2024-05-02 17:52:44 +0530

    bug fixed

--- controllers/CompareCsv/compareCsv.js
+++ controllers/CompareCsv/compareCsv.js
@@ -1,9 +1,8 @@
 const path = require("path");
 const fs = require("fs");
 const csvToJson = require("../../services/csvExtractor");
-const extractImagesFromZip = require("../../services/imageZipExtractor");
-
 const { parse } = require('json2csv');
+
 const compareCsv = async (req, res) => {
 
     // Access other form data parameters
@@ -12,11 +11,62 @@ const compareCsv = async (req, res) => {
     const secondCSVFile = req.uploadedFiles.secondInputCsvFile
     const firstFilePath = path.join(__dirname, "../", "../", "multipleCsvCompare", firstInputFileName);
     const secondFilePath = path.join(__dirname, "../", "../", "multipleCsvCompare", secondInputFileName);
-
+    console.log(firstInputFileName, secondInputFileName, primaryKey, skippingKey, imageColName)
     const f1 = await csvToJson(firstFilePath)
     const f2 = await csvToJson(secondFilePath)
 
     const diff = [];
+    // for (let i = 0; i < f1.length; i++) {
+    //     for (let j = 0; j < f2.length; j++) {
+    //         const pkLength = f1[i][primaryKey].length;
+    //         const str = " ".repeat(pkLength);
+
+    //         if (f1[i][primaryKey] === f2[j][primaryKey] && f1[i][primaryKey] !== str && f2[i][primaryKey] !== str) {
+    //             for (let [key, value] of Object.entries(f1[i])) {
+    //                 if (value !== f2[j][key]) {
+    //                     const val1 = value;
+    //                     const val2 = f2[j][key];
+    //                     const imgPathArr = f1[i][imageColName].split("\\");
+    //                     const imgName = imgPathArr[imgPathArr.length - 1]
+
+    //                     if (!skippingKey.includes(key)) {
+
+    //                         const obj = {
+    //                             "PRIMARY": ` ${f1[i][primaryKey]}`,
+    //                             "COLUMN_NAME": key,
+    //                             "FILE_1_DATA": val1,
+    //                             "FILE_2_DATA": val2,
+    //                             "IMAGE_NAME": imgName
+    //                         }
+    //                         diff.push(obj);
+    //                     }
+    //                 }
+    //             }
+    //         } else if (f1[i][primaryKey] === str && f2[i][primaryKey] !== str && i === j) {
+    //             for (let [key, value] of Object.entries(f1[i])) {
+    //                 if (value !== f2[j][key]) {
+    //                     const val1 = value;
+    //                     const val2 = f2[j][key];
+    //                     const imgPathArr = f1[i][imageColName].split("\\");
+    //                     const imgName = imgPathArr[imgPathArr.length - 1]
+
+    //                     if (!skippingKey.includes(key)) {
+
+    //                         const obj = {
+    //                             "PRIMARY": f1[i][primaryKey],
+    //                             "COLUMN_NAME": key,
+    //                             "FILE_1_DATA": val1,
+    //                             "FILE_2_DATA": val2,
+    //                             "IMAGE_NAME": imgName
+    //                         }
+    //                         diff.push(obj);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    // }
     for (let i = 0; i < f1.length; i++) {
         for (let j = 0; j < f2.length; j++) {
             const pkLength = f1[i][primaryKey].length;
@@ -27,7 +77,7 @@ const compareCsv = async (req, res) => {
                     if (value !== f2[j][key]) {
                         const val1 = value;
                         const val2 = f2[j][key];
-                        const imgPathArr = f1[i][imageColName].split("\\");
+                        const imgPathArr = f1[i][imageColName]?.split("\\");
                         const imgName = imgPathArr[imgPathArr.length - 1]
 
                         if (!skippingKey.includes(key)) {
@@ -71,7 +121,6 @@ const compareCsv = async (req, res) => {
 
 
 
-
     const csvData = parse(diff);
 
     const directoryPath = path.join(__dirname, "../", "../", 'ErrorCsv');
@@ -82,8 +131,7 @@ const compareCsv = async (req, res) => {
 
     // Specify the file path within the directory
     const filePath = path.join(directoryPath, 'output.csv');
-    // console.log(csvData);
-    // Write CSV data to a file
+
     fs.writeFile(filePath, csvData, (err) => {
         if (err) {
             console.error('Error writing CSV file:', err);
