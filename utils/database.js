bdca5a53a1bd8de5633816a5325cebea938fd8a1
Abhishek kumar <abhi.arya97161@gmail.com>
2024-05-30 11:07:16 +0530

    databaase logging changed

--- utils/database.js
+++ utils/database.js
@@ -20,6 +20,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize("omrscannerduplex", "root", "root", {
   dialect: "mysql",
   host: "localhost",
+  logging: false  
 });
 
 module.exports = sequelize;
