a62148bf2e18eb59ea72263444985c5fa1cac1f6
Abhishek kumar <abhi.arya97161@gmail.com>
2024-08-08 13:44:48 +0530

    permison given to moderator

--- controllers/userManagement/Alluser.js
+++ controllers/userManagement/Alluser.js
@@ -3,25 +3,28 @@ const User = require("../../models/User");
 const getUsers = async (req, res) => {
   const userRole = req.role;
   // console.log(req)
-  if(userRole !== "Admin"){
-    return res.status(500).json({msg: "Only Admin can create user"});
-  }
-  try {
-    const selectedAttributes = [
-      "id",
-      "userName",
-      "mobile",
-      "email",
-      "permissions",
-      "role",
-    ];
-    const users = await User.findAll({
-      attributes: selectedAttributes,
-    });
-    // console.log(users);
-    res.status(201).json({ message: "Users get successfully", users });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+  if (userRole == "Moderator" || userRole == "Admin") {
+    try {
+      const selectedAttributes = [
+        "id",
+        "userName",
+        "mobile",
+        "email",
+        "permissions",
+        "role",
+      ];
+      const users = await User.findAll({
+        attributes: selectedAttributes,
+      });
+      // console.log(users);
+      res.status(201).json({ message: "Users get successfully", users });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  } else {
+    return res
+      .status(500)
+      .json({ message: "Only Moderator or Admin can access this." });
   }
 };
 module.exports = getUsers;
